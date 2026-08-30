3b98307b94f9664c7383eb9499465905a8f6039c
Diego <diegolopez2003@gmail.com>
2026-08-30 11:24:51 -0400

    Complete CodePath iOS prework

--- IntroduceYourself/ViewController.swift
+++ IntroduceYourself/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    @IBOutlet weak var petsStepper: UIStepper!
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        petsStepper.minimumValue = 0
+        petsStepper.maximumValue = 100
+        petsStepper.stepValue = 1
+        numberOfPetsLabel.text = "\(Int(petsStepper.value))"
     }
 
+    @IBAction func stepperDidChange(_ sender: UIStepper) {
+        numberOfPetsLabel.text = "\(Int(sender.value))"
+    }
 
+    @IBAction func introduceSelfDidTapped(_ sender: UIButton) {
+        view.endEditing(true)
+
+        let firstName = firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        let lastName = lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        let schoolName = schoolNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex) ?? "First"
+        let numberOfPets = numberOfPetsLabel.text ?? "0"
+        let morePetsPreference = morePetsSwitch.isOn ? "would" : "would not"
+
+        let introduction = """
+        My name is \(firstName) \(lastName), and I attend \(schoolName).
+        I am currently in my \(year.lowercased()) year and I own \(numberOfPets) pets.
+        I \(morePetsPreference) like more pets.
+        """
+
+        let alertController = UIAlertController(
+            title: "My Introduction",
+            message: introduction,
+            preferredStyle: .alert
+        )
+        let action = UIAlertAction(title: "Nice to meet you!", style: .default)
+
+        alertController.addAction(action)
+        present(alertController, animated: true)
+    }
 }
-
---
+++ README.md
@@ -0,0 +1,24 @@
+# Introduce Yourself
+
+## App Description
+
+Introduce Yourself collects a student's first name, last name, school, academic year, number of pets, and pet preference. It then presents the information in a personalized introduction alert.
+
+## App Walk-through
+
+Walk-through GIF coming soon.
+
+## Required Features
+
+- [x] App displays an image of a school's logo
+- [x] App has three text fields for first, last, and school names
+- [x] App has a segmented control that changes the student's academic year
+- [x] The number-of-pets label is increased or decreased by a stepper
+- [x] A switch records whether the user wants more pets
+- [x] The Introduce Yourself button shows an alert with the introduction and a dismiss button
+
+## Optional Features
+
+- [ ] User can tap a button to change the background color
+- [ ] User can provide additional information through more controls
+- [ ] Additional custom styling
